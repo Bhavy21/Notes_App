319c2f463f0e82877172bbe6b53e6d7adaea5bb4
Bhavy21 <bhavyanathwani@gmail.com>
2025-05-31 00:38:16 +0530

    Mobile Optimization

--- client/src/components/EditorBox.jsx
+++ client/src/components/EditorBox.jsx
@@ -1,32 +1,13 @@
-import { useEffect, useCallback } from 'react';
 import JoditEditor from 'jodit-react';
-import debounce from 'lodash.debounce';
 
-const EditorBox = ({ editorRef, content, setContent, config }) => {
-  // Debounce inside component
-  const debouncedSetContent = useCallback(
-    debounce((newContent) => {
-      setContent(newContent);
-    }, 300),
-    [setContent]
-  );
-
-  // Cleanup on unmount to avoid memory leaks
-  useEffect(() => {
-    return () => {
-      debouncedSetContent.cancel();
-    };
-  }, [debouncedSetContent]);
-
-  return (
-    <JoditEditor
-      ref={editorRef}
-      value={content}
-      config={config}
-      tabIndex={1}
-      onChange={debouncedSetContent}
-    />
-  );
-};
+const EditorBox = ({ editorRef, content, setContent, config }) => (
+  <JoditEditor
+    ref={editorRef}
+    value={content}
+    config={config}
+    tabIndex={1}
+    onChange={(newContent) => setContent(newContent)}
+  />
+);
 
 export default EditorBox;
